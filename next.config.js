638ba1cded68e5ef2ff4e3d384b888b9a60788cb
DEVMANISHOFFl <devmanishofficial@gmail.com>
2025-07-07 23:29:55 +0530

    Install Netlify Next.js plugin

--- next.config.js
+++ next.config.js
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: 'export', // Enables static export
+
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: true, // Avoids breaking builds due to TS errors (use cautiously)
   },
+
   webpack(config) {
     config.module.rules.push({
       test: /pdf\.worker\.js$/,
-      type: 'asset/resource',
+      type: 'asset/resource', // Used for loading custom PDF workers
     });
     return config;
   },
